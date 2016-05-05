fca02b89a1eb12cba1f74b2db29ed1723422ccf3
renilsonmeneguci <renilsonandrade@hotmail.com>
2016-05-04 23:36:49 -0300

    Correction name

--- currency-all.js
+++ currency-all.js
@@ -9,14 +9,14 @@
  */
 
 jQuery.extend( jQuery.fn.dataTableExt.oSort, {
-   "br-currency-pre": function ( a ) {
+   "all-currency-pre": function ( a ) {
         a = (a==="-") ? 0 : parseFloat(a.replace( /[^\d\-\,]/g, "" ).replace( /,/g, "." ));
         return parseFloat( a );
     },
-    "br-currency-asc": function(a,b) {
+    "all-currency-asc": function(a,b) {
       return ((a < b) ? -1 : ((a > b) ? 1 : 0));
     },
-    "br-currency-desc": function(a,b) {
+    "ball-currency-desc": function(a,b) {
       return ((a < b) ? 1 : ((a > b) ? -1 : 0));
     }
 });
